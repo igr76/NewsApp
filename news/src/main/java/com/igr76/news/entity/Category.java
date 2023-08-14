1657f60a74cf7415e84f9bc16feff6140babacd3
igr76 <gritsuk766@gmail.com>
2023-08-14 19:11:02 +1000

    proseed

--- news/src/main/java/com/igr76/news/entity/Category.java
+++ news/src/main/java/com/igr76/news/entity/Category.java
@@ -1,12 +1,24 @@
 package com.igr76.news.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "categoryes")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    public String name;
+    @Column(name = "name")
+    private String name;
+
+
 }
--- news/src/main/java/com/igr76/news/entity/Feed.java
+++ news/src/main/java/com/igr76/news/entity/Feed.java
@@ -1,12 +1,28 @@
 package com.igr76.news.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "feeds")
 public class Feed {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     public String name;
+    @Column(name = "content")
+    private String content;
+    @Column(name = "data")
+    private LocalDateTime data;
+    @Column(name = "category_id")
+    private Category category;
 }
